599b19010040b36a69b66d87f62f44d2ea1efdd5
Kennedy Martin <kennedyjo21@gmail.com>
2019-09-22 15:23:19 -0700

    add business logic and variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
+// Business (or back-end) logic:
+
+var add = function(number1, number2, number3) {
+  return number1 + number2 + number3;
+};
+
+// Everything below this line is user interface (or front-end) logic:
+
+
 $(document).ready(function(){
   $("form#vacation").submit(function() {
     event.preventDefault();
     // var name = ($("#name").val());
     var age = parseInt($("#age").val());
+    var number1 = parseInt($("#question1").val());
+    var number2 = parseInt($("#question2").val());
+    var number3 = parseInt($("#question3").val());
+    var result = add(number1, number2, number3);
 
     if (age >= 70) {
       $('#elderly').show();
