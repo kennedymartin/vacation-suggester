8b475ba74d6dd1d85c89e53a6fcc22a9141b55bd
Kennedy Martin <kennedyjo21@gmail.com>
2019-09-22 17:34:23 -0700

    add jquery to hide answers not in use

--- js/scripts.js
+++ js/scripts.js
@@ -10,24 +10,42 @@ var add = function(number1, number2, number3) {
 $(document).ready(function(){
   $("form#vacation").submit(function() {
     event.preventDefault();
-    // var name = ($("#name").val());
     var age = parseInt($("#age").val());
     var number1 = parseInt($("#question4").val());
     var number2 = parseInt($("#question5").val());
     var number3 = parseInt($("#question6").val());
     var result = add(number1, number2, number3);
-    // $("#output").text(result);
 
     if (age >= 80) {
       $('#elderly').show();
+      $('#minor').hide();
+      $('#cabo').hide();
+      $('#sonoma').hide();
+      $('#ashland').hide();
     } else if (age < 18) {
       $('#minor').show();
+      $('#elderly').hide();
+      $('#cabo').hide();
+      $('#sonoma').hide();
+      $('#ashland').hide();
     } else if (result <= 4) {
       $('#cabo').show();
+      $('#elderly').hide();
+      $('#minor').hide();
+      $('#sonoma').hide();
+      $('#ashland').hide();
     } else if (result <=7 && result > 4) {
       $('#sonoma').show();
+      $('#elderly').hide();
+      $('#minor').hide();
+      $('#cabo').hide();
+      $('#ashland').hide();
     } else if (result > 7) {
       $('#ashland').show();
+      $('#elderly').hide();
+      $('#minor').hide();
+      $('#cabo').hide();
+      $('#sonoma').hide();
     }
 
   });
